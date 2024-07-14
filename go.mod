668049bb6656f5bf3cf5cb68a3c2925b148d7f52
Mohamad Fadhil <piukul@yahoo.com.my>
2024-07-15 00:55:36 +0800

    refactor to use redcon

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/sdil/durable-redis
 
 go 1.22.5
+
+require (
+	github.com/gookit/goutil v0.6.16 // indirect
+	github.com/tidwall/btree v1.1.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/redcon v1.6.2 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,72 +1,88 @@
 package main
 
 import (
-	"bufio"
-	"io"
+	"github.com/gookit/goutil/arrutil"
 	"log"
 	"net"
+	"strings"
+
+	"github.com/tidwall/redcon"
 )
 
-const (
-	port = "7781"
+var (
+	mutationCmds = []string{"set"}
 )
 
 func main() {
-	// Start a TCP server
-	address := "localhost:" + port
-	listener, err := net.Listen("tcp", address)
+	err := redcon.ListenAndServe("localhost:7781",
+		func(conn redcon.Conn, cmd redcon.Command) {
+			log.Printf("Received command: %s", string(cmd.Args[0]))
+
+			// Forward the command to the Redis server
+			resp, err := forwardToRedis(cmd)
+			if err != nil {
+				conn.WriteError(err.Error())
+				return
+			}
+
+			// Write the response back to the client
+			conn.WriteRaw(resp)
+
+			if arrutil.Contains(mutationCmds, strings.ToLower(string(cmd.Args[0]))) {
+				log.Printf("Publish command to Kafka")
+			}
+		},
+		func(conn redcon.Conn) bool {
+			// This is called when the client connects
+			log.Printf("Client connected: %s", conn.RemoteAddr())
+			return true
+		},
+		func(conn redcon.Conn, err error) {
+			// This is called when the client disconnects
+			log.Printf("Client disconnected: %s", conn.RemoteAddr())
+		},
+	)
+
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
-	defer listener.Close()
-	log.Println("Proxy listening on " + address)
-
-	// Accept connections
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Failed to accept connection: %v", err)
-			continue
-		}
-		go handleConnection(conn)
-	}
 }
 
-func handleConnection(clientConn net.Conn) {
-	defer clientConn.Close()
-
-	redisConn, err := net.Dial("tcp", "localhost:6379")
+func forwardToRedis(cmd redcon.Command) ([]byte, error) {
+	// Connect to the real Redis server
+	conn, err := net.Dial("tcp", "localhost:6379")
 	if err != nil {
-		log.Printf("Failed to connect to Redis server: %v", err)
-		return
+		return nil, err
 	}
-	defer redisConn.Close()
+	defer conn.Close()
 
-	go func() {
-		_, err := io.Copy(redisConn, clientConn)
-		if err != nil {
-			log.Printf("Error copying data from client to Redis: %v", err)
-		}
-	}()
+	// Write the command to the Redis server
+	_, err = conn.Write(cmd.Raw)
+	if err != nil {
+		return nil, err
+	}
 
-	reader := bufio.NewReader(redisConn)
-	writer := bufio.NewWriter(clientConn)
+	// Read the response from the Redis server
+	resp, err := readAll(conn)
+	if err != nil {
+		return nil, err
+	}
 
+	return resp, nil
+}
+
+func readAll(conn net.Conn) ([]byte, error) {
+	buf := make([]byte, 0, 4096)
+	tmp := make([]byte, 4096)
 	for {
-		line, err := reader.ReadBytes('\n')
+		n, err := conn.Read(tmp)
 		if err != nil {
-			if err != io.EOF {
-				log.Printf("Error reading from Redis connection: %v", err)
-			}
-			return
+			return nil, err
 		}
-		log.Printf("Received from server: %s", line)
-		_, err = writer.Write(line)
-		if err != nil {
-			log.Printf("Error writing to client connection: %v", err)
-			return
+		buf = append(buf, tmp[:n]...)
+		if n < len(tmp) {
+			break
 		}
-		log.Printf("Received from client: %s", line)
-		writer.Flush()
 	}
+	return buf, nil
 }
